21b8eb5f820d280e2e64f9bb86ceebc9cb2f9b2e
Tanyanyanya <76079713+Tanyanyanya@users.noreply.github.com>
2022-11-21 02:35:40 +0200

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -9,23 +9,23 @@ const router = new Map()
 const baseDir = path.join(__dirname, '/routes')
 
 async function loadRoutesDir(dirName, base) {
-  const relativePath = path.join(base, dirName)
-  const workDir = path.join(baseDir, relativePath)
+    const relativePath = path.join(base, dirName)
+    const workDir = path.join(baseDir, relativePath)
 
-  const dir = await readdir(workDir, { withFileTypes: true })
-  for (const dirent of dir) {
-    if (dirent.isDirectory()) {
-      return loadRoutesDir(dirent.name, path.join(base, dirName))
-    } else if (
-      dirent.isFile() &&
-      path.extname(dirent.name) === '.js' &&
-      path.basename(dirent.name, '.js') === 'index'
-    ) {
-      const modulePath = pathToFileURL(path.join(workDir, dirent.name))
-      const module = await import(modulePath)
-      router.set(relativePath.replaceAll(path.sep, '/'), { ...module })
+    const dir = await readdir(workDir, { withFileTypes: true })
+    for (const dirent of dir) {
+        if (dirent.isDirectory()) {
+            return loadRoutesDir(dirent.name, path.join(base, dirName))
+        } else if (
+            dirent.isFile() &&
+            path.extname(dirent.name) === '.js' &&
+            path.basename(dirent.name, '.js') === 'index'
+        ) {
+            const modulePath = pathToFileURL(path.join(workDir, dirent.name))
+            const module = await import(modulePath)
+            router.set(relativePath.replaceAll(path.sep, '/'), { ...module })
+        }
     }
-  }
 }
 
 await loadRoutesDir('', path.sep)
