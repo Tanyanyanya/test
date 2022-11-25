f00b09a0cd4e662441a2fa12242af4ea64e6b733
Tanyanyanya <76079713+Tanyanyanya@users.noreply.github.com>
2022-11-26 00:31:30 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,56 +1,53 @@
+import * as http from 'node:http'
 import router from './router.js'
 import defaultHandler from './defaultHandler.js'
-import * as http from 'node:http'
 import helpers from './helpers.js'
 import { safeJSON } from './utils.js'
 
-const processedContentType = {
-    'text/html': (text) => text,
-    'text/plain': (text) => text,
-    'application/json': (json) => safeJSON(json, {}),
-    'application/x-www-form-urlencoded': (data) =>
-        Object.fromEntries(new URLSearchParams(data)),
+const processedContentTypes = {
+  'text/html': (text) => text,
+  'text/plain': (text) => text,
+  'application/json': (json) => safeJSON(json, {}),
+  'application/x-www-form-urlencoded': (data) => {
+    return Object.fromEntries(new URLSearchParams(data))
+  },
 }
 
 const server = http.createServer(async (req, res) => {
-    const url = new URL(req.url || '/', `http://${req.headers.host}`)
-    const routerModule = router.get(url.pathname) ?? {}
-    const handler = routerModule[req?.method] ?? defaultHandler
+  const url = new URL(req.url || '/', `https://${req.headers.host}`)
+  const routerModule = router.get(url.pathname) ?? {}
+  const handler = routerModule[req?.method] ?? defaultHandler
+  let payload = {}
+  let rawRequest = ''
+  for await (const chunk of req) {
+    rawRequest += chunk
+  }
 
-    let payload = {}
-    let rawRequest = ''
-
-    for await (const chuck of req) {
-        rawRequest += chuck
+  if (req.headers['content-type']) {
+    const contentType = req.headers['content-type'].split(';')[0]
+    if (processedContentTypes[contentType]) {
+      payload = processedContentTypes[contentType](rawRequest)
     }
-
-    if (req.headers['content-type']) {
-        const contentType = req.headers['content-type'].split(';')[0]
-
-        if (processedContentType[contentType]) {
-            payload = processedContentType[contentType](rawRequest)
-        }
-    }
-    try {
-        handler(req, Object.assign(res, helpers), url, payload, rawRequest)
-    } catch (e) {
-        res.statusCode = 500
-        req.end(process.env.NODE_ENV === 'production' ? 'internal error' : e)
-    }
+  }
+  try {
+    handler(req, Object.assign(res, helpers), url, payload, rawRequest)
+  } catch (e) {
+    res.statusCode = 500
+    res.end(process.env.NODE_ENV === 'production' ? 'internal error' : e)
+  }
 })
 
 server.on('clientError', (err, socket) => {
-    if (err) console.log(err)
-    socket.end('HTTP/1.1 400 bad request\r\n\r\n')
+  if (err) console.log(err)
+  socket.end('HTTP/1.1 400 bad request\r\n\r\n')
 })
-
 server.listen(parseInt(process.env.PORT) || 8000)
 
 process.on('SIGINT', () => {
-    server.close((error) => {
-        if (error) {
-            console.log(error)
-            process.exit(1)
-        }
-    })
+  server.close((error) => {
+    if (error) {
+      console.error(error)
+      process.exit(1)
+    }
+  })
 })
