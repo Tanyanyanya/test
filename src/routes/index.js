ddd00b44c44baefb7edf534820e38d8f24e32a0a
Tanyanyanya <76079713+Tanyanyanya@users.noreply.github.com>
2022-11-26 00:34:35 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-function GET(req, res) {
-    res.json({ text: 'root get' })
-  }
-  
-  function OPTIONS(req, res) {
-    res.json({ name: 'root options' })
-  }
-  
-  function POST(req, res) {
-    res.json({ text: 'root post' })
-  }
-  
-  export { GET, OPTIONS, POST }
+function GET(req, res, url, payload) {
+  res.json({ text: 'root get' })
+}
+
+function OPTIONS(req, res) {
+  res.json({ text: 'root options' })
+}
+
+function POST(req, res, url, payload) {
+  res.json({ contentType: req.headers['content-type'], payload })
+}
+
+export { GET, OPTIONS, POST }
